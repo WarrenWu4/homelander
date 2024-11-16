89943f362168d525c1fb56c9ecca6698a9163111
WarrenWu4 <warrenweiwu04@gmail.com>
2024-11-16 17:53:40 -0600

    hungry monkey

---
+++ web3/src/Login.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+        <div>
+            View your home network connectivity and data!
+            <a href="/user">login</a>
+        </div>
+    )
+}
